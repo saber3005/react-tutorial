c0d6f9e8bdd01964c2f1e1a8439e6e520a7d7411
Eric Madar <ericm@spotify.com>
2016-11-17 17:26:35 -0800

    Add checkbox tests using enzyme

--- src/CheckboxWithLabel.js
+++ src/CheckboxWithLabel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class CheckboxWithLabel extends React.component {
+export default class CheckboxWithLabel extends React.Component {
 
     constructor(props) {
         super(props);
