cf9cd60bc3857d0ea7efa6e73222a548b6b8b126
Eric Madar <ericm@spotify.com>
2016-11-17 16:16:23 -0800

    change 'test' to 'it' to support IDE autocompletion

--- src/Link.test.js
+++ src/Link.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from './Link';
 import renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
+it('Link changes the class when hovered', () => {
     const component = renderer.create(
         <Link page="http://www.facebook.com">Facebook</Link>
     );
