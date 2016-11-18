99e0a877a7eb479301c0a27ef10789beb97edf85
Eric Madar <ericm@spotify.com>
2016-11-17 16:57:14 -0800

    Add more tests

--- src/Link.test.js
+++ src/Link.test.js
@@ -8,4 +8,16 @@ it('Link changes the class when hovered', () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    tree.props.onMouseEnter();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    tree.props.onMouseLeave();
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
 });
